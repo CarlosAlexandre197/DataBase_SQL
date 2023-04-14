e624833a84ffc0f81974c68650e784749ab79fb2
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-04-14 01:24:06 -0300

    Update Banco_Dados.py
    Edição do código.

--- Banco_Dados.py
+++ Banco_Dados.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-banco_db = sqlite3.connect('primeiro_banco.db')
+banco_db = sqlite3.connect('banco_dados.db')
 
 cursor = banco_db.cursor()
 
 cursor.execute('CREATE table pessoas (nome text, idade integer, email text)')
 
-cursor.execute('INSERT into pessoas values ("Alexandre", 35, "alexandre191@gmail.com")')
+cursor.execute('INSERT into pessoas values ("Alexandre", 35, "alexandrec191@gmail.com")')
 
 banco_db.commit()
 
