d1380d6b4dc03d8add3d90aed6a433b66a82fecb
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-04-18 01:57:20 -0300

    Add files via upload
    Atualização do código.

--- github_db.py
+++ github_db.py
@@ -7,6 +7,9 @@ cursor = banco.cursor()
 cursor.execute("CREATE TABLE lista (id integer, quantidade integer, descricao text, situacao text)")
 
 cursor.execute("INSERT INTO lista VALUES (4, 2, 'Óleo de soja 900ml', 'OK')")
+
+cursor.execute("DELETE from lista WHERE id = 4") # deletando um item da lista
  
 banco.commit()
+
 banco.close()
