63950836927fafdb53dc86f2a4b06bf86789f401
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-04-22 17:06:56 -0300

    Add files via upload
    Adicionando titulo para o código.

--- github_db.py
+++ github_db.py
@@ -1,3 +1,5 @@
+'''Lista de alimentos do nosso Chá de Panela'''
+
 import sqlite3
 
 banco = sqlite3.connect('github.db')
@@ -6,11 +8,11 @@ cursor = banco.cursor()
 
 cursor.execute("CREATE TABLE lista (id integer, quantidade integer, descricao text, situacao text)")
 
-cursor.execute("INSERT INTO lista VALUES (5, 30, 'Coxa e sobrecoxa kg', 'pendente')")
+cursor.execute("INSERT INTO lista VALUES (7, 3, 'Farinha de Mandioca kg', 'pendente')")
 
-cursor.execute("DELETE from lista WHERE id = 5") # deletando um item da lista
+cursor.execute("DELETE from lista WHERE id = 6") # deletando um item da lista
 
-cursor.execute("UPDATE lista SET situacao = 'pendente' WHERE id = 5") # atualizando um campo da lista
+cursor.execute("UPDATE lista SET descricao = 'Farinha de Mandioca 1kg' WHERE id = 7") # atualizando um campo da lista
  
 banco.commit()
 
