37e26946156dc213c758e8a85619f575aebc6e1a
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-04-27 00:51:00 -0300

    Add files via upload
    Adição de arquivo.

---
+++ exercicio_db.py
@@ -0,0 +1,15 @@
+'''Exercício'''
+
+import sqlite3
+
+conexao = sqlite3.connect('exercicio.db')
+
+cursor = conexao.cursor()
+
+#cursor.execute("CREATE TABLE cliente (id integer, nome (100), cpf text(11), data_cadastro text(10), endereço text(50))")
+
+cursor.execute("INSERT INTO cliente VALUES (1, 'Danilo', '11111111111', '05/10/2022', 'RIO TINTO')")
+
+conexao.commit
+
+conexao.close
