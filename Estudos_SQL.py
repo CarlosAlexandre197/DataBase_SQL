9e7659847e1c4f80cb4770074d0e06a1804c0632
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-05-22 09:23:42 -0300

    Add files via upload
    Atualização do código.

--- Estudos_SQL.py
+++ Estudos_SQL.py
@@ -20,7 +20,11 @@ import sqlite3
 conexao = sqlite3.connect('estudos.db')
 cursor = conexao.cursor()
 #cursor.execute("CREATE TABLE Cadastro_de_Entregas (id INTEGER NOT NULL PRIMARY KEY, Loja INTEGER, Destinatário VARCHAR(25), Telefone TEXT, UF TEXT, Município VARCHAR(25), Bairro VARCHAR(25), Número INTEGER, Endereço VARCHAR(250), CEP TEXT, Complemento TEXT, Data_Limite TEXT, Data_da_Carga TEXT, Volumes INTEGER, Valor_NF TEXT)")
-banco_de_dados = ("INSERT INTO Cadastro_de_Entregas (id, Loja, Destinatário, Telefone, UF, Município, Bairro, Número, Endereço, CEP, Complemento, Data_Limite, Data_da_Carga, Volumes, Valor_NF) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+# = ("INSERT INTO Cadastro_de_Entregas (id, Loja, Destinatário, Telefone, UF, Município, Bairro, Número, Endereço, CEP, Complemento, Data_Limite, Data_da_Carga, Volumes, Valor_NF) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+#valores = [id, loja, destinatario, telefone, uf, municipio, bairro, numero, endereco, cep, complemento, data_limite, data_da_carga, volumes, valor_nf]
+
+#cursor.execute("CREATE TABLE Cadastro_de_Entregas_Diario (id TEXT NOT NULL PRIMARY KEY, Loja INTEGER, Destinatário VARCHAR(25), Telefone TEXT, UF TEXT, Município VARCHAR(25), Bairro VARCHAR(25), Número INTEGER, Endereço VARCHAR(250), CEP TEXT, Complemento TEXT, Data_Limite TEXT, Data_da_Carga TEXT, Volumes INTEGER, Valor_NF TEXT)")
+banco_de_dados = ("INSERT INTO Cadastro_de_Entregas_Diario (id, Loja, Destinatário, Telefone, UF, Município, Bairro, Número, Endereço, CEP, Complemento, Data_Limite, Data_da_Carga, Volumes, Valor_NF) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
 valores = [id, loja, destinatario, telefone, uf, municipio, bairro, numero, endereco, cep, complemento, data_limite, data_da_carga, volumes, valor_nf]
 
 cursor.execute(banco_de_dados, valores)
