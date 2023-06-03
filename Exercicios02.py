8767e44f097a83b787151ff69e23fd76eb83d589
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-06-03 13:27:12 -0300

    Update Exercicios02.py
    Atualização.

--- Exercicios02.py
+++ Exercicios02.py
@@ -1,9 +1,9 @@
 '''Crie um programa em Python que gere tabelas para uma aplicação de gerenciamento de tarefas. 
 As tabelas devem compreender as seguintes funcionalidades:'''
 
-'''As tarefas devem ser divididas em “categorias”;
+'''>>>As tarefas devem ser divididas em “categorias”;
 Uma tarefa deve ter nome, data, categoria e status de conclusão (se foi realizada ou não); 
-As restrições/relacionamentos devem fazer sentido.'''
+As restrições/relacionamentos devem fazer sentido.<<<'''
 
 import sqlite3
 
