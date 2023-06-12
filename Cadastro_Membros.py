13e8e6f66d01931d8d83a126fd95c35036ae4892
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-06-11 23:43:27 -0300

    Add files via upload
    Novo arquivo.

---
+++ Cadastro_Membros.py
@@ -0,0 +1,25 @@
+'''Cadastro de Visitantes'''
+
+print('Vamos começar!!!')
+id = input('Data: ')
+nome = input('Nome: ')
+endereco = input('Endereço: ')
+cidade = input('Cidade: ')
+telefone = input('Telefone: ')
+whatsapp = input('Whatsapp: ')
+visitas = input('Deseja receber visitas? ')
+convertido = input('É novo convertido? ')
+
+import sqlite3
+
+conexao = sqlite3.connect('Cadastro_de_Membros.DB')
+cursor = conexao.cursor()
+
+cursor.execute("CREATE TABLE Cadastro_de_Membros (id TEXT, Nome TEXT, Endereço TEXT, Cidade TEXT, Telefone TEXT, Whatsapp TEXT, Visitas TEXT, Convertido)")
+planilha_de_dados = ("INSERT INTO Cadastro_de_Membros (id, Nome, Endereço, Cidade, Telefone, Whatsapp, Visitas, Convertido) VALUES(?, ?, ?, ?, ?, ?, ?, ?)")
+valores = [id, nome, endereco, cidade, telefone, whatsapp, visitas, convertido]
+
+cursor.execute(planilha_de_dados, valores)
+conexao.commit()
+print('<<<Dados inseridos com sucesso!>>>')
+conexao.close
