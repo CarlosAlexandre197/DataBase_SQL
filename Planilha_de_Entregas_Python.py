317aab4a40645ac04c0ac43a84ac2601ba8a2de0
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2024-03-23 09:28:07 -0300

    Atualizar o Planilha_de_Entregas_Python.py
    UPDATE

--- Planilha_de_Entregas_Python.py
+++ Planilha_de_Entregas_Python.py
@@ -1,7 +1,6 @@
 '''Planilha de Entregas Escritório 23/03/2024'''
 
 print('Vamos começar!!!')
-id = input()
 data = input('Data: ')
 taguatinga = int(input('Quantidade Taguatinga? '))
 ceilandia = int(input('Quantidade Ceilândia? '))
@@ -12,12 +11,12 @@ total = taguatinga + ceilandia + omni_channel
 
 import sqlite3
 
-conexao = sqlite3.connect('Planilha_Janeiro.2024.DB')
+conexao = sqlite3.connect('Planilha_marco_2024.DB')
 cursor = conexao.cursor()
 
 cursor.execute("CREATE TABLE Planilha_de_Entregas (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Data TEXT, Taguatinga INTEGER, Ceilândia INTEGER, Omni_Channel INTEGER, Assinatura VARCHAR(25), Total TEXT)")
-planilha_de_dados = ("INSERT INTO Planilha_de_Entregas (id, Data, Taguatinga, Ceilândia, Omni_Channel, Assinatura, Total) VALUES(?, ?, ?, ?, ?, ?, ?)")
-valores = [id, data, taguatinga, ceilandia, omni_channel, assinatura, total]
+planilha_de_dados = ("INSERT INTO Planilha_de_Entregas (Data, Taguatinga, Ceilândia, Omni_Channel, Assinatura, Total) VALUES(?, ?, ?, ?, ?, ?)")
+valores = [data, taguatinga, ceilandia, omni_channel, assinatura, total]
 
 cursor.execute(planilha_de_dados, valores)
 conexao.commit()
