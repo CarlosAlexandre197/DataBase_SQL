ac3003e9fdbd4c4b62b24c9d3d6eb631c866b469
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2025-11-01 09:01:57 -0300

    Atualizar o Planilha_de_Entregas_Python.py
    Update

--- Planilha_de_Entregas_Python.py
+++ Planilha_de_Entregas_Python.py
@@ -1,4 +1,4 @@
-'''Planilha de Entregas Escritório 2025-10-31'''
+'''Planilha de Entregas Escritório 2025-11-01'''
 
 print('Vamos começar?')
 data = input('Data: ')
@@ -11,7 +11,7 @@ total = taguatinga + ceilandia + omni_channel
 
 import sqlite3
 
-conexao = sqlite3.connect('Planilha_outubro_2025.DB')
+conexao = sqlite3.connect('Planilha_novembro_2025.DB')
 cursor = conexao.cursor()
 
 cursor.execute("CREATE TABLE Planilha_de_Entregas (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Data TEXT, Taguatinga INTEGER, Ceilândia INTEGER, Omni_Channel INTEGER, Assinatura VARCHAR(25), Total TEXT)")
